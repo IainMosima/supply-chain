c81c7d1320da03f948e19fb640c8bb05af84f753
IainMosima <iainmosima@gmail.com>
2023-06-20 11:17:31 +0300

    remived build file

--- frontend/.next/static/chunks/app/contact-us/page-65c03d40d4cf7df8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[746],{509:function(n,e,u){Promise.resolve().then(u.bind(u,125))}},function(n){n.O(0,[853,125,667,139,744],function(){return n(n.s=509)}),_N_E=n.O()}]);
