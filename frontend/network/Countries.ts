4d9e628c78dcbf0d5cd1a8cd93144e273256679d
IainMosima <iainmosima@gmail.com>
2023-10-28 13:09:16 +0300

    api function call in countries

--- frontend/network/Countries.ts
+++ frontend/network/Countries.ts
@@ -2,6 +2,6 @@ import { Country } from "@/models/Country";
 import { fetchData } from "./FetchData";
 
 export async function getAllCountries(): Promise<Country[]> {
-    const response = await fetchData(`${process.env.BACKENDIP}/api/unsecured/countries`, { method: 'GET', cache: 'no-store' });
+    const response = await fetchData(`/api/unsecured/countries`, { method: 'GET', cache: 'no-store' });
     return response.json();
 }
